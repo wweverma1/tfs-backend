d418fc18cc4d7fddbba67660272e41006cd08b70
wweverma1 <wweverma1@gmail.com>
2021-09-12 17:59:37 +0530

    created model file for all the tables

---
+++ app/bookings/models.py
@@ -0,0 +1,49 @@
+# Standard library imports
+from datetime import datetime
+import traceback
+
+# Related third party imports
+from sqlalchemy.exc import SQLAlchemyError
+
+# Local app specific imports
+from app import (
+    db,
+    SCHEMA_NAME,
+)
+
+
+class Booking(db.Model):
+    __table_args__ = ({"Schema": SCHEMA_NAME})
+    __tablename__ = 'booking'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey(f'{SCHEMA_NAME}.user.id'))
+    user_name = db.Column(db.String(100))
+    movie_id = db.Column(db.Integer, db.ForeignKey(f'{SCHEMA_NAME}.movie.id'))
+    movie_name = db.Column(db.String(100))
+    screening_id = db.Column(db.Integer, db.ForeignKey(f'{SCHEMA_NAME}.screening.id'))
+    screening_time = db.Column(db.DateTime)
+    number_seats = db.Column(db.Integer)
+
+    @staticmethod
+    def create_booking(user_id=None, user_name=None, movie_id=None, movie_name=None, screening_id=None, screening_time=None, number_seats=None):
+        try:
+            booking = Booking(
+                created_at=datetime.now(),
+                user_id=user_id,
+                user_name=user_name,
+                movie_id=movie_id,
+                movie_name=movie_name,
+                screening_id=screening_id,
+                screening_time=screening_time,
+                number_seats=number_seats
+            )
+            db.session.add(booking)
+            db.session.commit()
+            return booking
+        except SQLAlchemyError as e:
+            print(e)
+            traceback.print_exc()
+            db.session.rollback()
+            return False
---
+++ app/otp/models.py
@@ -0,0 +1,49 @@
+# Standard library imports
+from datetime import datetime
+from datetime import timedelta
+import traceback
+import random
+import string
+
+# Related third party imports
+from sqlalchemy.exc import SQLAlchemyError
+
+# Local app specific imports
+from app import (
+    db,
+    SCHEMA_NAME,
+)
+
+
+class OTP(db.Model):
+    __table_args__ = ({"Schema": SCHEMA_NAME})
+    __tablename__ = 'otp'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime)
+    created_for = db.Column(db.Integer, db.ForeignKey(f'{SCHEMA_NAME}.user.id'))
+    otp = db.Column(db.String(6))
+    expiry = db.Column(db.DateTime)
+
+    @staticmethod
+    def generate_otp(user_id=None):
+        try:
+            size = 6
+            code = ''.join([random.choice( string.ascii_uppercase +
+                                            string.ascii_lowercase +
+                                            string.digits)
+                                            for n in range(size)])
+            otp = OTP(
+                created_at=datetime.now(),
+                created_for=user_id,
+                otp=code,
+                expiry=datetime.now()+timedelta(minutes = 10),
+            )
+            db.session.add(otp)
+            db.session.commit()
+            return otp
+        except SQLAlchemyError as e:
+            print(e)
+            traceback.print_exc()
+            db.session.rollback()
+            return False
