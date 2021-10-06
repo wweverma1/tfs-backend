07fd19e90ddb81736a4a900784893a5060d2b83b
wweverma1 <wweverma1@gmail.com>
2021-10-06 11:07:37 +0530

    updated welcome message

--- app/home/controller.py
+++ app/home/controller.py
@@ -1,3 +1,3 @@
 def home():
-    welcome_message = "Welcome to TFS APIs 🙏 <br/> GitHub Repository Link : https://github.com/wweverma1/tfs-backend <br/> Made with ❤️ by Aditya Verma"
+    welcome_message = "Welcome to TFS APIs 🙏 <br/> GitHub Repository Link : https://github.com/wweverma1/tfs-backend <br/> Developed with ❤️ by Aditya Verma"
     return welcome_message, 200
